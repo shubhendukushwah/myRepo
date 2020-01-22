04f0aa164a1f8f71b2eb839df39eaf9a47fdb8c2
shubhendu <skushwah@bestpeers.com>
2020-01-22 18:07:10 +0530

    Problem #05 by Shubhendu

---
+++ Problem_05.py
@@ -0,0 +1,16 @@
+class String():
+    def getString(self):
+        x=input()
+        return x
+
+    def printString(self):
+        print(self.getString().upper())
+
+
+obj = String()
+
+obj.printString()
+
+
+
+
